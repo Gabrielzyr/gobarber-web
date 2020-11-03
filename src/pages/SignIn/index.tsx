9931b24f081d4db962692ae51e81196f2b83a60c
Gabrielzyr <gabrielmz397@gmail.com>
2020-11-03 12:04:21 -0300

    Added ToastContainer structure

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -10,7 +10,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Background, Container, Content } from './styles';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 interface SignInFormData {
@@ -43,9 +43,12 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
       } catch (err) {
-        const errors = getValidationErrors(err);
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
 
-        formRef.current?.setErrors(errors);
+          formRef.current?.setErrors(errors);
+        }
+        // toast here
       }
     },
     [signIn],
