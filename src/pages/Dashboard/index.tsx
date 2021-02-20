58d0d81e0c5911fca2cc629527f9de8ceb7dfaf9
GabrielZyr <64475947+Gabrielzyr@users.noreply.github.com>
2021-02-19 22:19:53 -0300

    Added Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,68 @@
 import React from 'react';
+import { FiClock, FiPower } from 'react-icons/fi';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContainer,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
   return (
     <Container>
-      <h1>adasda</h1>
-      <strong>adfsdf</strong>
+      <Header>
+        <HeaderContainer>
+          <img src={logoImg} alt="Gobarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button onClick={signOut} type="button">
+            <FiPower size={24} />
+          </button>
+        </HeaderContainer>
+      </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars.githubusercontent.com/u/2254731?s=460&u=4fcc8ca9672eeb41ea800271831b7c687bc17054&v=4"
+                alt="Diego Fernandes"
+              />
+
+              <strong>Diego Fernandes</strong>
+              <span>
+                <FiClock />
+                17:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
